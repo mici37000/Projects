360804890f1683a0e59c05f1def3146d205f377b
Michael Margolis <michaelmargolispa@gmail.com>
2020-04-30 22:17:36 +0300

    delete everithing

--- 1-friend_query/scripts/util.js
+++
@@ -1,6 +0,0 @@
-function generateHTML (tpl,data){
-    var placeHolderReg = /{([^}]+)}/g;
-    return tpl.replace(placeHolderReg,function(match, p1, offset, string){
-        return data[p1]
-    });
-}
--- 3-leaderboard/lang/text_en.js
+++
@@ -1,6 +0,0 @@
-tPT = window.tPT || {};
-//L is Leaderboard
-tPT['L1'] = "<b>Scoreboard:</b> during the event time, we will publish a real-time scoreboard right here! To learn about your current rank visit this page, choose the relevant competition section and keep track of your progress!";
-tPT['L2A'] = "The scoreboard is updated every ";
-tPT['L2B'] = " minutes.";
-tPT['L3'] = "Last updated: ";
